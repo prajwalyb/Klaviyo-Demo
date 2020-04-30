da001138b6c4b32af9e1cc1020d940ebf89f3087
alankritgupta091099 <alankritgupta091099@gmail.com>
2020-04-30 23:10:09 +0530

    navbar redesigned

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -21,10 +21,11 @@ const NavComp = (props) => {
         <div className="container-fluid">
             <Navbar light expand="md" >
                 <div className="header-secondary-container">
-                    <NavbarBrand href="/" style={{color: '#DFE3E6'}}>Klaviyo</NavbarBrand>
+                    <NavbarBrand href="/React-DND-Flowchart" style={{color: '#DFE3E6'}}>Klaviyo</NavbarBrand>
                     <input
                         type="text"
                         placeholder="Search for Someone"
+                        className="navsearch"
                     />
                 </div>
                 <div className="header-primary-container">
@@ -32,13 +33,16 @@ const NavComp = (props) => {
                 <Collapse isOpen={isOpen} navbar className="nav">
                     <Nav className="ml-auto Nav" navbar>
                         <NavItem className="NavItem">
-                            <NavLink className="NavLink" href="/">Support</NavLink>
-                        </NavItem>            
-                        <NavItem className="NavItem">
-                            <NavLink href="/" className="NavLink">Blog</NavLink>
+                            <NavLink href="/React-DND-Flowchart" className="NavLink">Upgrade</NavLink>
                         </NavItem>
                         <NavItem className="NavItem">
-                            <NavLink href="/" className="NavLink">Account</NavLink>
+                            <NavLink className="NavLink" href="/React-DND-Flowchart">Support</NavLink>
+                        </NavItem>            
+                        <NavItem className="NavItem">
+                            <NavLink href="/React-DND-Flowchart" className="NavLink">Blog</NavLink>
+                        </NavItem>
+                        <NavItem className="NavItem">
+                            <NavLink href="/React-DND-Flowchart" className="NavLink">Account</NavLink>
                         </NavItem>                        
                     </Nav>
                 </Collapse>
