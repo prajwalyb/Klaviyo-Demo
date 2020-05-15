1c3900c30df912b50886fb8428b4572249cdbf96
alankritgupta091099 <alankritgupta091099@gmail.com>
2020-05-16 01:04:34 +0530

    flow backend almost complete

--- routes/flows.js
+++ routes/flows.js
@@ -15,4 +15,31 @@ router.post('/save',(req,res)=>{
         .catch(err=>{console.log(err)});
 })
 
+router.get('/loadAll/:user_id', async (req,res)=>{
+    let flows= await Flow.find({ "user._id":req.params.user_id})
+    if(flows)
+        return res.status(200).json(flows);
+    return null
+})
+
+router.get('/:user_id/loadOne/:flow_id', (req,res)=>{
+    Flow.findOne({"user._id":req.params.user_id,"flow.flow_id":req.params.flow_id})
+        .then(flow=>{
+            return res.status(200).json(flow)
+        })
+        .catch(err=>{
+            return res.status(404).json({msg:"Not Found"})
+        })
+})
+
+router.delete('/:user_id/deleteOne/:flow_id', (req,res)=>{
+    Flow.find({"user._id":req.params.user_id , "flow.flow_id":req.params.flow_id}).deleteOne()
+        .then(status=>{          
+            if(status.deletedCount==1)
+                return res.status(200).json({success:true})
+            return res.status(404).json({success:false})
+        })
+        .catch(err=>res.status(404).json({success:false}));
+})
+
 module.exports= router;
